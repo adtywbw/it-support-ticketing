e77c8549cc2d4d6ec823f52eb6fdb7f71a5544c1
adtywbw <adityawibowo004@gmail.com>
2026-07-04 18:00:32 +0700

    feat(db): add Faq model and migration

---
+++ backend/prisma/migrations/20260704105850_add_faq/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "faqs" (
+    "id" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "display_order" INTEGER NOT NULL DEFAULT 0,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "faqs_pkey" PRIMARY KEY ("id")
+);
